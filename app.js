b5a54be5cf2878c059e42e1de698d4b7bfbba2e2
SushantPokhrel <sushant.805421@cct.tu.edu.np>
2024-10-24 12:03:07 +0545

    modified features

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const api_key = "AIzaSyBCI_aDAA1M7lKHgMXkyHRkHVd0JRIIfyo";
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${api_key}`;
+// please do not misuse this apiKey.
 const input = document.querySelector(".typing-input");
 const submit_btn = document.querySelector("#send-message-button");
 const form = document.querySelector(".typing-form");
@@ -7,12 +8,15 @@ const chat_container = document.querySelector(".chat-list");
 let user_prompt = null;
 let is_loading = null;
 let result = null;
+const loader = document.createElement("p");
 form.addEventListener("submit", () => {
   user_prompt = input.value;
   fetchRequest(user_prompt);
   input.value = "";
   if (!is_loading) {
-    chat_container.innerHTML = `<p>Generating response...</p>`;
+    loader.textContent = "Generating response...";
+    // chat_container.innerHTML = `<p>Generating response...</p>`;
+    chat_container.appendChild(loader);
   }
 });
 
@@ -37,7 +41,14 @@ async function fetchRequest(prompt) {
     result = data.candidates[0].content.parts[0].text;
     if (result) {
       is_loading = false;
-      chat_container.innerHTML = `<h3>${prompt}</h3><p>${result}</p>`;
+      const h3 = document.createElement("h3");
+      const p = document.createElement("p");
+      h3.textContent = `${prompt}`;
+      p.textContent = `${result}`;
+      loader.textContent = "";
+      chat_container.appendChild(h3);
+      chat_container.appendChild(p);
+      // `<h3>${prompt}</h3><p>${result}</p>`;
     } else {
       is_loading = true;
     }
@@ -45,3 +56,7 @@ async function fetchRequest(prompt) {
     console.log(error);
   }
 }
+const delete_btn = document.querySelector("#delete-chat-button");
+delete_btn.addEventListener("click", () => {
+  chat_container.textContent = "";
+});
